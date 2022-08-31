501f007387684ece482f7925e4adf8bae8c3537c
Chris-Lazarius <Lazariuscm@gmail.com>
2022-08-31 11:12:34 +0200

    Angular Fronted has bee modified, json fl now publ

--- src/main/java/com/cyborgue/app/rest/RestApplication.java
+++ src/main/java/com/cyborgue/app/rest/RestApplication.java
@@ -7,10 +7,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.cyborgue.app.rest.Models.Student;
 import com.cyborgue.app.rest.Repo.UserRepo;
-
 
 @RestController
 @SpringBootApplication
